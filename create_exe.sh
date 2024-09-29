3e5248b0dfc5ce662e927b8fd817575d299ee70e
mohammadmansour200 <abdultawab940@gmail.com>
2024-09-29 11:41:52 +0300

    fix: some typos in shell script

--- create_exe.sh
+++ create_exe.sh
@@ -6,7 +6,7 @@ if python -c "import sys; exit(0 if sys.version_info <= (9, ) else 1)"; then
   # Make a virtual enviroment
   pip install virtualenv && python -m venv venv
 
-  if [[ "$OSTYPE" == "msys" ]]; then
+  if [ "$OSTYPE" = "msys" ]; then
     source venv/Scripts/activate
   else
     source venv/bin/activate
@@ -15,10 +15,8 @@ if python -c "import sys; exit(0 if sys.version_info <= (9, ) else 1)"; then
   # Install needed packages and create the executable
   pip install spleeter pyinstaller && pyinstaller --onefile execute_spleeter.py
 
-  echo "
-  Executable is located in the dist folder.
-  Jazak-allah khair for removing music from media files! enjoy ;)
-  "
+  echo "Executable is located in the dist folder.
+  Jazak-allah khair for removing music from media files! enjoy ;)"
 
 else
   echo "Spleeter requires a python version lower than 3.9"
